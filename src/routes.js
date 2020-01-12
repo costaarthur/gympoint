f5737b5795890e6dfd5bebbae7729420dd8fab5e
costaarthur <costaarthur@hotmail.com>
2020-01-12 14:34:32 -0300

    gg pt 1

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,20 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+// routes.put('/users', authMiddleware, UserController.update);
+// da para utilizar o middleware assim/\ de forma local
+
+// ou de forma global assim. Ai funciona em apenas nas rotas abaixo dele
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;
