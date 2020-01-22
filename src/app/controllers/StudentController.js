642215f38dad341a773c1bbebe5648af5d59625c
costaarthur <costaarthur@hotmail.com>
2020-01-22 11:04:02 -0300

    yup added gg all

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -1,8 +1,30 @@
+import * as Yup from 'yup';
 import Student from '../models/Student';
 
 class StudentController {
   async store(req, res) {
-    // const { id, name, email, provider } = await User.create(req.body);
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      age: Yup.number().required(),
+      weight: Yup.number().required(),
+      height: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const studentExists = await Student.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (studentExists) {
+      return res.status(400).json({ error: 'Email already exists' });
+    }
+
     const { id, name, email, age, weight, height } = await Student.create(
       req.body
     );
@@ -18,11 +40,21 @@ class StudentController {
   }
 
   async update(req, res) {
-    const { email } = req.body;
-    // const student = await Student.findByPk(req.studentId);
-    // const student = await Student.findByPk(req.studentId);
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string()
+        .email()
+        .required(),
+      age: Yup.number(),
+      weight: Yup.number(),
+      height: Yup.number(),
+    });
 
-    // console.log(student);
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { email } = req.body;
 
     const student = await Student.findOne({ where: { email } });
 
